74282bb57577bcba10abbdcffcbaf746d208c8b1
Erik Tryggvason <theros@node-1w7jr9qpwjspysly4lw8a0yi2.ipv6.telus.net>
2017-08-16 16:40:44 -0700

    Initial Commit

---
+++ PassKee/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PassKee
+//
+//  Created by Erik Tryggvason on 2017-08-16.
+//  Copyright © 2017 theros. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
